a9d13237e2784ad4771d526a436a1ad834b4b20b
Vadim <vadim@knodeit.com>
2015-08-24 10:55:13 +0300

    changed config

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://localhost/dolphin-vadim-dev',
+    db: 'mongodb://localhost/dolphin-dev',
     debug: true,
     mongoose: {
         debug: false
