0c9eb62f93865f093ae1006aeae40d28f12f9ef7
Vadim <vadim@knodeit.com>
2015-08-28 15:21:38 +0300

    deleted trash

--- packages/custom/blog/server/config/env/all.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var rootPath = require('path').normalize(__dirname + '/../../..');
-
-module.exports = {
-    root: rootPath
-};
--- packages/custom/blog/server/config/env/development.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    amqp: 'amqp://blog:blog@localhost:5672',
-    queues: {
-        blogProcessing: 'blog.converting'
-    }
-};
